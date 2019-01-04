251cb37644636fda206e3f6e555d9faef9c35ae7
Grizwold <tomekbielaszewski@gmail.com>
2019-01-04 22:05:47 +0100

    parsing and saving planet data from AJAX response

--- scanner.user.script.js
+++ scanner.user.script.js
@@ -7,11 +7,17 @@
 // @match        https://www.ugamela.pl/s1/galaxy.php*
 // @resource     scanner-ui https://raw.githubusercontent.com/tomekbielaszewski/ugamela-galaxy-scanner/master/scanner.html
 // @grant        GM_getResourceText
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
 (function () {
   'use strict';
   const SCANNER_UI = 'scanner-ui';
+
+  function planetID(galaxy, system, planet) {
+    return `${galaxy}:${system}:${planet}`
+  }
 
 
   (function () {
@@ -25,11 +31,70 @@
     function attachAjaxListener() {
       $(document).ajaxComplete(function (event, xhr, settings) {
         if (settings.url.startsWith('ajax/galaxy.php') && xhr.status === 200) {
-          $(JSON.parse(xhr.responseText).Data).each(function() {
-            console.log($(this).html())
-          });
+          let response = JSON.parse(xhr.responseText);
+          saveSystem(response.G, response.S, response.Data);
         }
       });
+    }
+
+    function saveSystem(galaxy, system, planets) {
+      $(planets).each(function () {
+        savePlanet(galaxy, system, this);
+      });
+    }
+
+    function savePlanet(galaxy, system, planetRow) {
+      let planetNumber = getPlanetNumber(planetRow);
+      let playerName = getPlanetOwner(planetRow);
+      let playerActivity = getPlayerActivity(planetRow);
+      let hasMoon = hasMoon(planetRow);
+      let debrisFieldType = getDebrisFieldType(planetRow);
+      let playerRank = getPlayerRank(planetRow);
+      let alliance = getAlliance(planetRow);
+      let rawHTML = planetRow.html();
+
+      let scannerData = GM_getValue(SCANNER_DATA, {});
+      scannerData[planetID(galaxy, system, planetNumber)] = {
+        galaxy,
+        system,
+        planetNumber,
+        playerName,
+        hasMoon,
+        debrisFieldType,
+        playerRank,
+        alliance,
+        playerActivity,
+        rawHTML
+      };
+      GM_setValue(SCANNER_DATA, scannerData);
+    }
+
+    function getPlanetNumber(planetRow) {
+      return undefined;
+    }
+
+    function getPlanetOwner(planetRow) {
+      return undefined;
+    }
+
+    function getPlayerActivity(planetRow) {
+      return undefined;
+    }
+
+    function hasMoon(planetRow) {
+      return undefined;
+    }
+
+    function getDebrisFieldType(planetRow) {
+      return undefined;
+    }
+
+    function getPlayerRank(planetRow) {
+      return undefined;
+    }
+
+    function getAlliance(planetRow) {
+      return undefined;
     }
   })();
 })();
