9b135066e335559e595c46ef4d97f640c2679bab
tbielaszewski <tbielaszewski@objectivity.co.uk>
2019-01-04 14:59:35 +0100

    userscript template corrected with Ugamela values

--- scanner.user.script.js
+++ scanner.user.script.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
+// @name         Ugamela
+// @namespace    Ugamela Galaxy Scanner
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.ugamela.pl/s1/overview.php
+// @description  Saves galaxy state while browsing
+// @author       tomekbielaszewski
+// @match        https://www.ugamela.pl/s1/galaxy.php*
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @resource     html https://raw.githubusercontent.com/tomekbielaszewski/ugamela-galaxy-scanner/master/scanner.html
 // @grant        none
 // ==/UserScript==
 
