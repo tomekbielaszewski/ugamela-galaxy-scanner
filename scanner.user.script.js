a82b3b0fd5a116852d7ea9ba9b75ace0def9ae48
Grizwold <tomekbielaszewski@gmail.com>
2019-01-04 20:26:58 +0100

    loading scanner UI onto the screen

--- scanner.user.script.js
+++ scanner.user.script.js
@@ -1,17 +1,21 @@
 // ==UserScript==
-// @name         Ugamela
-// @namespace    Ugamela Galaxy Scanner
+// @name         Ugamela Galaxy Scanner
+// @namespace    Ugamela
 // @version      0.1
 // @description  Saves galaxy state while browsing
 // @author       tomekbielaszewski
 // @match        https://www.ugamela.pl/s1/galaxy.php*
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
-// @resource     html https://raw.githubusercontent.com/tomekbielaszewski/ugamela-galaxy-scanner/master/scanner.html
-// @grant        none
+// @resource     scanner-ui https://raw.githubusercontent.com/tomekbielaszewski/ugamela-galaxy-scanner/master/scanner.html
+// @grant        GM_getResourceText
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  // Your code here...
+  loadUI();
+
+  function loadUI() {
+    $('#gameContent > center > table').append($(GM_getResourceText('scanner-ui')))
+  }
 })();
